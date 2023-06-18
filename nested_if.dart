32192bc4f5471b7afbc06a8cc7ef3f1e21c5d5d9
usmanghani2023 <usman.efulife@gmail.com>
2023-06-18 12:18:06 -0700

    practice of nested if

---
+++ nested_if.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num = 12;
+  if (num % 3 == 0 || num % 4 == 0) {
+    if (num % 3 == 0 && num % 4 == 0) {
+      print("num is divisible by both");
+    } else if (num % 3 == 0) {
+      print("number is divisle by 3");
+    } else {
+      print("number is divisible by 4");
+    }
+  } else {
+    print("number is not divisible by 3 and 4");
+  }
+}
